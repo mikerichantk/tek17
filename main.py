e051a2bdfe41f168c2fa7d69162fb97ba33308c6
baldwinn20 <42782171+baldwinn20@users.noreply.github.com>
2021-03-16 10:13:20 -0700

    This doesn't work rn

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ __credits__ = ["Addison Raak", "Michael Antkiewicz", "Ka'ulu Ng", "Nicholas Bald
 
 import sys
 import os
-import random
+import numpy as np
 import matplotlib
 import cv2
-from PyQt5.QtWidgets import QFrame, QHBoxLayout
+from PyQt5 import Qt, QtGui
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QFrame, QHBoxLayout, QVBoxLayout, QApplication, QLabel
 
 matplotlib.use('Qt5Agg')
 
 from graph_widget import DrawGraph
-from PyQt5 import Qt
+
 from numpy import arange, sin, pi
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, pyqtSlot
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
@@ -120,13 +122,32 @@ class OverlayWidget(QtWidgets.QWidget):
         print(e.key())
 
 
+#code sourced from https://gist.github.com/docPhil99/ca4da12c9d6f29b9cea137b617c7b8b1
+class VideoThread(QThread):
+    change_pixmap_signal = pyqtSignal(np.ndarray)
+
+    def __init__(self):
+        super().__init__()
+        self._run_flag = True
+
+    def run(self):
+        # capture from web cam
+        cap = cv2.VideoCapture("rtsp://169.254.161.100:554/user=admin_password=tlJwpbo6_channel=1_stream=0.sdp?real_stream")
+        while self._run_flag:
+            ret, cv_img = cap.read()
+            if ret:
+                self.change_pixmap_signal.emit(cv_img)
+        # shut down capture system
+        cap.release()
+
+    def stop(self):
+        """Sets run flag to False and waits for thread to finish"""
+        self._run_flag = False
+        self.wait()
+
+
 # Class for the Side-by-Side Tab
 class SideBySideWidget(QtWidgets.QWidget):
-
-    # add spot for camera feed
-    # layout_container.addWidget()
-    #################################
-
 
     def __init__(self):
         super(QtWidgets.QWidget, self).__init__()
@@ -175,7 +196,7 @@ class SideBySideWidget(QtWidgets.QWidget):
         layout_container.addWidget(self.graph_widget.graph_canvas, 0, 0, 1, 3)
 
         #add temporary box where video feed will go
-        videoBox = QHBoxLayout()
+        videoBox = QVBoxLayout()
         videoFrame = QFrame(self)
         videoFrame.setFrameShape(QFrame.StyledPanel)
         videoFrame.setLineWidth(0.6)
@@ -183,6 +204,45 @@ class SideBySideWidget(QtWidgets.QWidget):
         videoBox.addWidget(videoFrame)
         self.setLayout(videoBox)
 
+        self.display_width = 640
+        self.display_height = 480
+        self.image_label = QLabel(self)
+        self.image_label.resize(self.display_width, self.display_height)
+        # create a text label
+        self.textLabel = QLabel('Webcam')
+
+        ###sourced from https://gist.github.com/docPhil99/ca4da12c9d6f29b9cea137b617c7b8b1###
+        # create a vertical box layout and add the two labels
+        videoBox.addWidget(self.image_label)
+        videoBox.addWidget(self.textLabel)
+        # set the vbox layout as the widgets layout
+        self.setLayout(videoBox)
+
+        # create the video capture thread
+        self.thread = VideoThread()
+        # connect its signal to the update_image slot
+        #self.thread.change_pixmap_signal.connect(self.update_image)
+        # start the thread
+        self.thread.start()
+
+        def closeEvent(self, event):
+            self.thread.stop()
+            event.accept()
+
+        @pyqtSlot(np.ndarray)
+        def update_image(self, cv_img):
+            """Updates the image_label with a new opencv image"""
+            qt_img = self.convert_cv_qt(cv_img)
+            self.image_label.setPixmap(qt_img)
+
+        def convert_cv_qt(self, cv_img):
+            """Convert from an opencv image to QPixmap"""
+            rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+            h, w, ch = rgb_image.shape
+            bytes_per_line = ch * w
+            convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
+            p = convert_to_Qt_format.scaled(self.display_width, self.display_height, Qt.KeepAspectRatio)
+            return QPixmap.fromImage(p)
 
     def click_up(self):
         print("Up button was pressed.")
@@ -201,6 +261,8 @@ class SideBySideWidget(QtWidgets.QWidget):
 
     def click_zoom_out(self):
         print("Zoom out button pressed.")
+
+
 
 
 # Main class for adding components to the application
@@ -228,4 +290,12 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     # runs AppMainWindow class as main container
     main_window = AppMainWindow()
+    a = SideBySideWidget()
+    a.show()
     sys.exit(app.exec_())
+
+
+    #app = QApplication(sys.argv)
+    #a = SideBySideWidget()
+    #a.show()
+    #sys.exit(app.exec_())
